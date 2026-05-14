d463d3170c4eb73ed01cf0e42f9a863f6220620b
Vidhyanshu Kumar <vidhyanshukumarvidhyam@gmail.com>
2026-05-14 16:25:41 +0530

    UI: change night mode color scheme to black and grey

--- app/page.tsx
+++ app/page.tsx
@@ -88,11 +88,11 @@ export default function Home() {
       if (window.pageYOffset > 100) {
         header.style.backgroundColor = isDayMode
           ? 'rgba(255, 255, 255, 0.7)'
-          : 'rgba(26, 35, 50, 0.7)'
+          : 'rgba(10, 10, 10, 0.7)'
       } else {
         header.style.backgroundColor = isDayMode
           ? 'rgba(255, 255, 255, 0.4)'
-          : 'rgba(26, 35, 50, 0.4)'
+          : 'rgba(10, 10, 10, 0.4)'
       }
 
       header.style.backdropFilter = 'blur(15px) saturate(180%)'
