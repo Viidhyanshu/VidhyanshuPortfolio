179e53148ee1927131c82f03af453955fd7f73c2
Vidhyanshu Kumar <vidhyanshukumarvidhyam@gmail.com>
2026-05-15 14:17:54 +0530

    Implement animated lighting effect in footer

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -3,6 +3,18 @@ import React from 'react';
 const Footer: React.FC = () => {
   return (
     <footer className="footer">
+      <article id="lightings">
+        <section id="lighting-one" className="lighting-section">
+          <section id="lighting-two" className="lighting-section">
+            <section id="lighting-three" className="lighting-section">
+              <section id="lighting-four" className="lighting-section">
+                <section id="lighting-five" className="lighting-section">
+                </section>
+              </section>
+            </section>
+          </section>
+        </section>
+      </article>
       <div className="container">
         <p>© 2025 Vidhyanshu Kumar. All rights reserved.</p>
       </div>
